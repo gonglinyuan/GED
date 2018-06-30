b7bd0c4ab9054eb090971f3b604dcd16a9bd4d82
Gong Linyuan <gonglinyuan@hotmail.com>
2018-06-30 12:48:48 +0800

    begin evaluation

--- data/evalutaion.py
+++ data/evalutaion.py
@@ -1,31 +1,19 @@
 import os
 import sys
+from subprocess import run
 
-name = sys.argv[1]
 dataset = ""
-if len(sys.argv) >= 3: dataset = sys.argv[2]
+if len(sys.argv) >= 2: dataset = sys.argv[1]
 
 dirs = os.listdir("testdata")
 
 for test_set_name in dirs:
-	if dataset not in test_set_name: continue
-	test_path = "testdata/" + test_set_name
-	datas = os.listdir(test_path)
-	datas = sorted(datas)
-	result = []
-	for data_name in datas:
-		data_path = test_path + "/" + data_name
-		print(data_path)
-		os.system("../cmake-build-debug/" + name + " 2 4 1 1 " + data_path + "/1.gxl " + data_path + "/2.gxl > result.out")
-		ans = int(open("result.out", "r").readline()[: -1])
-		print(ans)
-		result.append(ans)
-
-	result_dir = "result/" + name
-	if not os.path.exists(result_dir):
-		os.makedirs(result_dir)
-	output_file = open(result_dir + "/" + test_set_name + ".out", "w")
-	for i in result:
-		output_file.write(str(i) + "\n")	
-	output_file.close()
-		
+    if dataset not in test_set_name: continue
+    test_path = "testdata/" + test_set_name
+    datas = os.listdir(test_path)
+    datas = sorted(datas)
+    result = []
+    for data_name in datas:
+        data_path = test_path + "/" + data_name
+        print(data_path)
+        run(["../GED", "2", "4", "1", "1", data_path + "/1.gxl ", data_path + "/2.gxl"])
